54523f2dee792788fa01bc0662fdb45229e827d7
Roppii <ng.roche7@gmail.com>
2019-10-10 19:06:04 +0200

    Collection system

---
+++ Assets/MAIN/Script/Collectibles/CollectionSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script should be attached to the collectible prefab and uses GameManager as a parameter.
+/// </summary>
+
+public class CollectionSystem : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            gameManager.Collectibles++;
+            Destroy(this.gameObject);
+
+            Debug.Log("Feather collected. You have " + gameManager.Collectibles + " feathers in total.");
+        }
+    }
+}
---
+++ Assets/MAIN/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int _collectibles = 0;
+
+    public int Collectibles {
+        get {
+            return _collectibles;
+        }
+
+        set {
+            if (value > 0) {
+                _collectibles = value;
+            }
+        }
+    }
+}
