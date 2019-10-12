a80a35950249f1607a5eda5731a03f4f70289b20
Roppii <ng.roche7@gmail.com>
2019-10-12 22:20:20 +0200

    First version menu added

---
+++ Assets/MAIN/Script/Menu/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Text VolumeNumber;
+    public Slider Volume;
+    public AudioSource MusicTheme;
+    public GameObject MenuCanvas;
+    public GameObject SettingsCanvas;
+
+    public void NewGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void VolumeUpdate(float VolumeValue) {
+        VolumeNumber.text = Mathf.RoundToInt(VolumeValue * 100) + "%";
+        MusicTheme.volume = Volume.value;
+    }
+}
