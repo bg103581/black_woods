27d5a79de1f4b5100584928d4ab4157a2c9ac371
Byzness <guillaume.briant@free.fr>
2019-11-09 17:59:11 +0100

    ControlerInput
    Gamepad controls (need to have a gamepad and the keyboard to play Strix and Dots)

--- Assets/MAIN/Script/Player/Dots.cs
+++ Assets/MAIN/Script/Player/Dots.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Dots : Player
 {
     private void Awake() {
         Init();
 
-        _controls.Player.DotsMovement.performed += ctx => _move = new Vector2(ctx.ReadValue<float>(), 0);
-        _controls.Player.DotsMovement.canceled += ctx => _move = Vector2.zero;
+        //_controls.Player.DotsMovement.performed += ctx => _move = new Vector2(ctx.ReadValue<float>(), 0);
+        //_controls.Player.DotsMovement.canceled += ctx => _move = Vector2.zero;
 
-        _controls.Player.DotsJump.performed += ctx => Jump();
+        //_controls.Player.DotsJump.performed += ctx => Jump();
     }
 
     private void Update() {
         PlayerUpdate();
+    }
+
+    private void OnDotsMovement(InputValue value) {
+        _move = new Vector2(value.Get<Vector2>().x, 0);
+    }
+
+    private void OnDotsJump() {
+        if (_isGrounded) {
+            //make the jump
+            _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+            _rb.velocity += Vector3.up * _jumpStrength;
+        }
     }
 }
---
+++ Assets/MAIN/Script/Player/TrainPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TrainPlayer : MonoBehaviour
+{
+    Vector3 move;
+    [SerializeField]
+    private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+
+    void OnMovement(InputValue value) {
+        Debug.Log("moving");
+        move = new Vector3(value.Get<Vector2>().x, value.Get<Vector2>().y, 0);
+    }
+}
