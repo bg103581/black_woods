d2bf50d74880be3a1256fe63c9b0f4d7ad6f7df0
Roppii <ng.roche7@gmail.com>
2019-12-17 12:40:28 +0100

    Description triggers added

---
+++ Assets/MAIN/Script/DialogueSystem/TriggerObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour
+{
+    public bool isTriggered = false;
+    public bool countPlayers;
+
+    [SerializeField]
+    private int playerCount = 0;
+    private bool strixHasPassed = false;
+    private bool dotsHasPassed = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if (!countPlayers) {
+            if (!isTriggered) {
+                if (other.tag == "Player") {
+                    isTriggered = true;
+                }
+            }
+        } else {
+            if (other.tag == "Player") {
+
+                if (playerCount < 2) {
+
+                    if (((other.name == "Strix") & !strixHasPassed)) {
+                        strixHasPassed = true;
+                        playerCount++;
+                        if ((playerCount == 2) && !isTriggered) {
+                            isTriggered = true;
+                        }
+                    } else {
+                        if (((other.name == "Dots") & !dotsHasPassed)) {
+                            dotsHasPassed = true;
+                            playerCount++;
+                            if ((playerCount == 2) && !isTriggered) {
+                                isTriggered = true;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
