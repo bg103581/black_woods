7fd9ed98d8a55cd62cf55fc0e159e7c805565f51
Yoann <yomaisonneuve@gmail.com>
2020-01-07 00:46:05 +0100

    Strix control the swing

--- Assets/MAIN/Script/Environment/SwingController.cs
+++ Assets/MAIN/Script/Environment/SwingController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class SwingController : MonoBehaviour
@@ -9,6 +8,13 @@ public class SwingController : MonoBehaviour
     private const float RETURN_SWING_SPEED = 0.05f;
     private float _timeSinceStrixLeft;
     private float _timeSinceStrixOn = 1;
+    
+    [Range(-90,0)] private float angleLeftToReach = -20;
+    [Range(0,90)] private float angleRightToReach = 20;
+
+    private Strix strix;
+    private float currentAngleLeftToReach = -20;
+    private float currentAngleRightToReach = 20;
     private enum SwingPhase
     {
         TO_RIGHT,
@@ -27,8 +33,12 @@ public class SwingController : MonoBehaviour
     {
         if(_playerDetector.GetStrix()!=null)
         {
+            strix = _playerDetector.GetStrix().GetComponent<Strix>();
             _timeSinceStrixLeft = 0;
+            _timeSinceStrixOn += Time.deltaTime;
             UpdateSwingPhase();
+            UpdateAngleToReach();
+            UpdateCurrentAngleToReach();
             UpdateSwingRotation();
         }
         else
@@ -42,16 +52,31 @@ public class SwingController : MonoBehaviour
     {
         switch (currentSwingPhase)
         {
-            case SwingPhase.STOP:
-                currentSwingPhase = SwingPhase.TO_RIGHT;
+            case SwingPhase.STOP :
+                if(angleRightToReach != 0)
+                {
+                    currentSwingPhase = SwingPhase.TO_RIGHT;
+                }
+                else if(angleLeftToReach != 0)
+                {
+                    currentSwingPhase = SwingPhase.TO_RIGHT;
+                }
                 break;
-            case SwingPhase.TO_RIGHT when transform.rotation.z >= Quaternion.Euler(0,0,85).z:
+            case SwingPhase.TO_RIGHT when IsNear(transform.rotation.eulerAngles.z,currentAngleRightToReach,5):
+                print("ReachRight");
+                _timeSinceStrixOn = 0;
                 currentSwingPhase = SwingPhase.TO_LEFT;
                 break;
-            case SwingPhase.TO_LEFT when transform.rotation.z <= Quaternion.Euler(0,0,-85).z:
+            case SwingPhase.TO_LEFT when IsNear(transform.rotation.eulerAngles.z - 360,currentAngleLeftToReach,5):
+                print("ReachLeft");
+                _timeSinceStrixOn = 0;
                 currentSwingPhase = SwingPhase.TO_RIGHT;
                 break;
-            default:
+            default :
+                if (IsNear(angleLeftToReach,angleRightToReach,0) && IsNear(angleLeftToReach,0,0))
+                {
+                    currentSwingPhase = SwingPhase.STOP;
+                }
                 break;
         }
     }
@@ -61,16 +86,49 @@ public class SwingController : MonoBehaviour
         switch (currentSwingPhase)
         {
             case SwingPhase.TO_RIGHT :
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,90),RETURN_SWING_SPEED * _timeSinceStrixOn);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,currentAngleRightToReach),RETURN_SWING_SPEED * _timeSinceStrixOn);
                 break;
             case SwingPhase.TO_LEFT :
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,-90),RETURN_SWING_SPEED * _timeSinceStrixOn);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,currentAngleLeftToReach),RETURN_SWING_SPEED * _timeSinceStrixOn);
                 break;
             case SwingPhase.STOP:
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+    private void UpdateCurrentAngleToReach()
+    {
+        if (currentSwingPhase == SwingPhase.TO_RIGHT && IsNear(transform.rotation.eulerAngles.z,0,2))
+        {
+            currentAngleRightToReach = angleRightToReach;
+        }
+
+        if (currentSwingPhase == SwingPhase.TO_LEFT && IsNear(transform.rotation.eulerAngles.z, 0, 2))
+        {
+            currentAngleLeftToReach = angleLeftToReach;
+        }
+    }
+
+    private void UpdateAngleToReach()
+    {
+        if (strix.GetMoveX() > 0 && transform.rotation.z < 0)
+        {
+            angleRightToReach += 20 * Time.deltaTime;
+            angleLeftToReach += 4 * Time.deltaTime;
+        }
+        else if (strix.GetMoveX() < 0 && transform.rotation.z > 0)
+        {
+            angleLeftToReach -= 20 * Time.deltaTime;
+            angleRightToReach -= 4 * Time.deltaTime;
+        }
+        else
+        {
+            angleLeftToReach += 4 * Time.deltaTime;
+            angleRightToReach -= 4 * Time.deltaTime;
+        }
+        
     }
     
     private void BackToOriginRotation()
@@ -84,6 +142,15 @@ public class SwingController : MonoBehaviour
         else
         {
             currentSwingPhase = SwingPhase.STOP;
+            angleLeftToReach = -40;
+            angleRightToReach = 40;
+            currentAngleLeftToReach = -40;
+            currentAngleRightToReach = 40;
         }
+    }
+
+    private bool IsNear(float a, float b, float error)
+    {
+        return Mathf.Abs(a - b) < error;
     }
 }
