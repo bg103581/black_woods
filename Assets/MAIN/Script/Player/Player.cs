2d3b2099487ad9cf0c90a8aefa13ded7a67c8ad2
Guillaume Briant <guillaume.briant@free.fr>
2020-01-08 22:41:46 +0100

    Distance Max between Players

--- Assets/MAIN/Script/Player/Player.cs
+++ Assets/MAIN/Script/Player/Player.cs
@@ -6,18 +6,24 @@ using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
     protected bool _isGrounded;
     protected bool _isGroundCollided;
     protected bool _onTree;
     protected bool _wallClimb;
     protected bool _isNextToHole;
+    private bool _goRightWhenMaxLeft;
+    private bool _goLeftWhenMaxRight;
+    [HideInInspector]
+    public bool IsLeft;
 
+    [HideInInspector]
     public Vector2 _move;
 
     private float _oldX;
     protected float _moveX;
     protected float _moveY;
+
+    private static CameraControl _cameraControl;
 
     protected Rigidbody _rb;
 
@@ -48,33 +54,36 @@ public class Player : MonoBehaviour
     //features that has to be put in update, common with Dots and Strix
     protected void PlayerUpdate() {
         _isGrounded = (Physics.OverlapSphere(transform.position + _bottomOffset, _collisionRadius, _groundLayer).Length != 0) && _isGroundCollided;
+        _goRightWhenMaxLeft = (_cameraControl.IsToFar && IsLeft && (_moveX > 0));
+        _goLeftWhenMaxRight = (_cameraControl.IsToFar && !IsLeft && (_moveX < 0));
 
         if (_isGrounded || _wallClimb) {
             //walk
-            _rb.velocity = new Vector3(_move.x * _playerSpeed, _rb.velocity.y, _rb.velocity.z);
-
-            if (_rb.velocity.x < 0 && transform.rotation == Quaternion.Euler(0,90f,0)) {
-                transform.rotation = Quaternion.Euler(0, 270f, 0);
-
+            if (!_cameraControl.IsToFar || _goRightWhenMaxLeft || _goLeftWhenMaxRight) {
+                _rb.velocity = new Vector3(_moveX * _playerSpeed, _rb.velocity.y, _rb.velocity.z);
             }
-            else if (_rb.velocity.x > 0 && transform.rotation == Quaternion.Euler(0, 270f, 0)) {
+
+            if (_moveX < 0 && transform.rotation == Quaternion.Euler(0,90f,0)) {
+                transform.rotation = Quaternion.Euler(0, 270f, 0);
+            }
+            else if (_moveX > 0 && transform.rotation == Quaternion.Euler(0, 270f, 0)) {
                 transform.rotation = Quaternion.Euler(0, 90f, 0);
             }
 
             //dots utilise son bec
             if (_wallClimb) {
-                _rb.velocity = new Vector3(0, _move.y * _playerSpeed, _rb.velocity.z);
+                _rb.velocity = new Vector3(0, _moveY * _playerSpeed, _rb.velocity.z);
             }
-            _oldX = _move.x;
+            _oldX = _moveX;
         }
         else {
             //aircontrol
-            if (_oldX != _move.x) {
-                if (_move.x > 0 && _rb.velocity.x < _playerSpeed)
+            if (_oldX != _moveX) {
+                if (_moveX > 0 && _rb.velocity.x < _playerSpeed)
                     _rb.AddForce(Vector3.right * _airControl, ForceMode.Force);
-                else if (_move.x < 0 && _rb.velocity.x > (-1 * _playerSpeed))
+                else if (_moveX < 0 && _rb.velocity.x > (-1 * _playerSpeed))
                     _rb.AddForce(Vector3.left * _airControl, ForceMode.Force);
-                else if (_move.x == 0 && Mathf.Abs(_rb.velocity.x) > 0.5f) {
+                else if (_moveX == 0 && Mathf.Abs(_rb.velocity.x) > 0.5f) {
                     if (_oldX < 0) {
                         _rb.AddForce(Vector3.right * _airControl, ForceMode.Force);
                     }
@@ -95,6 +104,7 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
         animator = GetComponent<Animator>();
+        _cameraControl = FindObjectOfType<CameraControl>();
     }
 
     protected void Move(InputValue value) {
