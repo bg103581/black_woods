fcd80a528872420d61e3d8ecf3332c19e94bbcd2
Guillaume Briant <guillaume.briant@free.fr>
2020-02-05 01:21:19 +0100

    Transitions between scenes

---
+++ Assets/MAIN/Script/Environment/LoadSceneOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _crossFade;
+    private bool _isTriggered;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            if (!_isTriggered) {
+                _isTriggered = true;
+                StartCoroutine("SwitchSceneEndChapter1");
+            }
+        }
+    }
+
+    IEnumerator SwitchSceneEndChapter1() {
+        _crossFade.SetTrigger("crossFadeTrigger");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ Assets/MAIN/Script/GameManager/CinematicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicManager : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _crossFade;
+    [SerializeField]
+    private int _sceneNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("SwitchSceneCinematic");
+    }
+
+    IEnumerator SwitchSceneCinematic() {
+        yield return new WaitForSeconds(4f);
+        _crossFade.SetTrigger("crossFadeTrigger");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(_sceneNumber);
+    }
+}
