8aff36f1ec5bd6e9e34931816bc18a24c2f3633b
Roppii <ng.roche7@gmail.com>
2020-02-08 00:11:56 +0100

    Dialogues added

--- Assets/MAIN/Script/GameManager/CinematicManager.cs
+++ Assets/MAIN/Script/GameManager/CinematicManager.cs
@@ -9,15 +9,21 @@ public class CinematicManager : MonoBehaviour
     private Animator _crossFade;
     [SerializeField]
     private int _sceneNumber;
+    private DialogueManager dialogueManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("SwitchSceneCinematic");
+        dialogueManager = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>();
+    }
+
+    private void Update() {
+        if (dialogueManager.isReadyForNextScene) {
+            StartCoroutine("SwitchSceneCinematic");
+        }
     }
 
     IEnumerator SwitchSceneCinematic() {
-        yield return new WaitForSeconds(4f);
         _crossFade.SetTrigger("crossFadeTrigger");
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(_sceneNumber);
