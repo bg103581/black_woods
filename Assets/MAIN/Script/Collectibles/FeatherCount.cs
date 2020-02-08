2dd4a10ffee87e17ad7c56c4df46f6f5428b7d31
Roppii <ng.roche7@gmail.com>
2020-02-09 00:46:21 +0100

    bug fix

---
+++ Assets/MAIN/Script/Collectibles/FeatherCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FeatherCount : MonoBehaviour
+{
+    public TextMeshProUGUI featherCountText;
+
+    private void Update() {
+        featherCountText.text = GameManager.Instance.Collectibles.ToString();
+    }
+}
