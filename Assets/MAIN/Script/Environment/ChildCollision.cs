3be5b71c480436d377851884327bf09e28c5bd34
Roppii <ng.roche7@gmail.com>
2020-02-13 10:07:59 +0100

    Cave scene

---
+++ Assets/MAIN/Script/Environment/ChildCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) {
+        transform.parent.GetComponent<ActivateObject>().CollisionDetected(this, collision);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        transform.parent.GetComponent<ActivateObject>().TriggerDetected(this, other);
+    }
+}
