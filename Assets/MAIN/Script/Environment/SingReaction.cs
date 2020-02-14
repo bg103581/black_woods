9c36efe4015787b1cb0ac4785fb940bd3b6c1d11
Roppii <ng.roche7@gmail.com>
2020-02-14 07:48:17 +0100

    Firefly added + singing skill

---
+++ Assets/MAIN/Script/Environment/SingReaction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SingReaction : MonoBehaviour {
+    [SerializeField]
+    private Transform _fireflyPos;
+
+    public void React() {
+        if (name == "Firefly") {
+            transform.SetParent(_fireflyPos);
+            transform.DOJump(_fireflyPos.position, 0, 1, 2f).OnComplete(() => EndMoveToStrixHead());
+        }
+    }
+
+    private void EndMoveToStrixHead() {
+        //animator.SetBool("isJumping", false);
+        transform.position = _fireflyPos.position;
+        transform.rotation = _fireflyPos.rotation;
+    }
+}
